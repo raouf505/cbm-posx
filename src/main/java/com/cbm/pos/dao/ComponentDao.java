316c4727b2633ee27079ee76cc7fdafbb0f51c42
chrisbol82 <chrisbol82@gmail.com>
2012-09-04 22:02:19 +0000

    Domain, service and dao component objects created

---
+++ src/main/java/com/cbm/pos/dao/ComponentDao.java
@@ -0,0 +1,5 @@
+package com.cbm.pos.dao;
+
+public class ComponentDao {
+
+}
---
+++ src/main/java/com/cbm/pos/service/ComponentServiceImp.java
@@ -0,0 +1,15 @@
+package com.cbm.pos.service;
+
+import com.cbm.pos.domain.Component;
+
+public class ComponentServiceImp implements ComponentService {
+	public Component searchBySize(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Component add(Component component) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
