6a552ca656fdcddf291127a13478812d252d8ecd
chrisbol82 <chrisbol82@gmail.com>
2013-08-23 03:29:22 +0000

    1. Board domain object created. 2. TableHandlerController name updated to BoardHandlerController.

---
+++ src/main/java/com/cbm/pos/web/domain/Board.java
@@ -0,0 +1,14 @@
+package com.cbm.pos.web.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TABLE")
+public class Board implements Serializable {
+	
+	private static final long serialVersionUID = -5527566248002296042L;
+	
+}
