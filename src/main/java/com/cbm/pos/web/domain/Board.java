e74c29b283a580e275c3aa48be88013a04f89867
chrisbol82 <chrisbol82@gmail.com>
2013-08-24 21:16:02 +0000

    Board domain object added

--- src/main/java/com/cbm/pos/web/domain/Board.java
+++ src/main/java/com/cbm/pos/web/domain/Board.java
@@ -2,13 +2,49 @@ package com.cbm.pos.web.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "TABLE")
+@Table(name = "BOARD")
 public class Board implements Serializable {
 	
 	private static final long serialVersionUID = -5527566248002296042L;
+	
+	@Id
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "POSX")
+	private int posX;
+	
+	@Column(name = "POSY")
+	private int posY;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
 	
 }
