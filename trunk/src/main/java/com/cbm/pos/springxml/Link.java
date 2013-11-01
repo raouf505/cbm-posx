f81a0f3848442365b5ec0321e9377aebd981b464
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2013-11-01 22:28:02 +0000

    Jibx removed and JAXB configured instead. Default locale modified to "es_CR"

---
+++ trunk/src/main/java/com/cbm/pos/springxml/Link.java
@@ -0,0 +1,38 @@
+package com.cbm.pos.springxml;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class Link {
+
+	private String id;
+	private String text;
+	private String href;
+	
+	@XmlElement(name="id")
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@XmlElement(name="text")
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	@XmlElement(name="href")
+	public String getHref() {
+		return href;
+	}
+	
+	public void setHref(String href) {
+		this.href = href;
+	}
+	
+}
---
+++ trunk/src/main/java/com/cbm/pos/springxml/Navigation.java
@@ -0,0 +1,22 @@
+package com.cbm.pos.springxml;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlRootElement(name="navigation")
+public class Navigation {
+	
+	private ArrayList<Link> linkList;
+
+	@XmlElement(name="link")
+	public ArrayList<Link> getLinkList() {
+		return linkList;
+	}
+
+	public void setLinkList(ArrayList<Link> linkList) {
+		this.linkList = linkList;
+	}
+
+}
