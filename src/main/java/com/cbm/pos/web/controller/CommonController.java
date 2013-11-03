065983efcccd9192087299cf882e125d1b648c10
chrisbol82 <chrisbol82@gmail.com>
2013-11-03 21:05:32 +0000

    Global navigation defined as model attribute

--- src/main/java/com/cbm/pos/web/controller/CommonController.java
+++ src/main/java/com/cbm/pos/web/controller/CommonController.java
@@ -1,13 +1,22 @@
 package com.cbm.pos.web.controller;
 
-import javax.inject.Inject;
-import javax.inject.Named;
+import java.util.Locale;
 
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.cbm.pos.springxml.Navigation;
 import com.cbm.pos.web.service.ContentManagerService;
 
 public class CommonController {
 	
 	@Inject
 	protected ContentManagerService contentManagerService;
+	
+	@ModelAttribute
+	public Navigation navigation(Locale locale) {
+		return contentManagerService.getContent(locale.toString(), "navigation", Navigation.class);
+	}
 	
 }
