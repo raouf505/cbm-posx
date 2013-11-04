6d658af05e9c23d1050c40abe2280ee7d50a477c
chrisbol82 <chrisbol82@gmail.com>
2013-11-04 14:30:06 +0000

    "public-resources" folder name updated to "resources"

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,12 @@
+// Filename: app.js
+define(["jquery", "underscore", "backbone", "marionette"], function($, _, Backbone, Marionette) {
+	
+	var initialize = function(){
+		var App = new Marionette.Application();
+		
+	};
+
+	return {
+		initialize: initialize
+	};
+});
