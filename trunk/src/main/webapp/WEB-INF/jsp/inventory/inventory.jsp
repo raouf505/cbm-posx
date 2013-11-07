e5359e87ea0340fbeaa21ce5789054cc768db924
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2013-11-07 22:20:00 +0000

    Change backbone/marionette code in order to get data from DB instead of using dummy data.

--- trunk/src/main/webapp/WEB-INF/jsp/inventory/inventory.jsp
+++ trunk/src/main/webapp/WEB-INF/jsp/inventory/inventory.jsp
@@ -2,6 +2,4 @@
 
 <h2>${ pageContent.title }</h2>
 
-<div id="main-region" class="container">
-	<p>Here is static content in the web page. You'll notice that it gets replaced by our app as soon as we start it.</p>
-</div>
+<div id="inventoryItems"></div>
--- trunk/src/main/webapp/resources/js/apps/InventoryApp.js
+++ trunk/src/main/webapp/resources/js/apps/InventoryApp.js
@@ -1,60 +1,24 @@
-define(["jquery", "underscore", "backbone", "marionette", "views/inventoryItemView", "tpl!templates/inventoryItemTemplate.html"], function($, _, Backbone, Marionette, inventoryItemView, inventoryItemTemplate) {
+define(["jquery", "underscore", "backbone", "marionette", "views/InventoryItemCollectionView", "collections/InventoryItemCollection"], function($, _, Backbone, Marionette, InventoryItemCollectionView, InventoryItemCollection) {
 	
 	var InventoryApp = new Marionette.Application();
 
 	InventoryApp.addRegions({
-		mainRegion : "#main-region"
-	});
-
-	InventoryApp.Contact = Backbone.Model.extend({});
-
-	InventoryApp.ContactCollection = Backbone.Collection.extend({
-		model : InventoryApp.Contact
-	});
-
-	InventoryApp.ContactItemView = Marionette.ItemView.extend({
-		template : inventoryItemTemplate
-	});
-
-	InventoryApp.ContactsView = Marionette.CollectionView.extend({
-		tagName : "ul",
-		itemView : InventoryApp.ContactItemView
+		inventoryItems : "#inventoryItems"
 	});
 
 	InventoryApp.on("initialize:after", function() {
-		var contacts = new InventoryApp.ContactCollection([ {
-			firstName : "Bob",
-			lastName : "Brigham",
-			phoneNumber : "555-0163"
-		}, {
-			firstName : "Alice",
-			lastName : "Arten",
-			phoneNumber : "555-0184"
-		}, {
-			firstName : "Charlie",
-			lastName : "Campbell",
-			phoneNumber : "555-0129"
-		} ]);
-
-		var contactsListView = new InventoryApp.ContactsView({
-			collection : contacts
+		var inventoryItemCollectionView;
+		var inventoryItemCollection = new InventoryItemCollection();
+		inventoryItemCollection.fetch({
+			success: function() {
+				inventoryItemCollectionView = new InventoryItemCollectionView({
+					collection: inventoryItemCollection
+				});
+				
+				InventoryApp.inventoryItems.show(inventoryItemCollectionView);
+			}
 		});
 
-		InventoryApp.mainRegion.show(contactsListView);
-		
-//			var TodosCollection = Backbone.Collection.extend({
-//				model: InventoryApp.Contact,
-//				url: '/inventory/crudService'
-//			});
-//			
-//			var todos = new TodosCollection();
-//			todos.fetch({success: function(){
-//				var contactsListView = new InventoryApp.ContactsView({
-//					collection : todos
-//				});
-//	
-//				InventoryApp.mainRegion.show(contactsListView);
-//			}});
 	});
 
 		
