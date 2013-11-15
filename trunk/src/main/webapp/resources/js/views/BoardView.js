cc7a0d492d519be14f579cb1aa2ee8648e1dce3c
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2013-11-15 03:55:13 +0000

    Drag/drop board functionality added

--- trunk/src/main/webapp/resources/js/views/BoardView.js
+++ trunk/src/main/webapp/resources/js/views/BoardView.js
@@ -1,8 +1,18 @@
 define(["marionette", "tpl!templates/boardTemplate.html"], function(Marionette, template) {
 	
 	return Marionette.ItemView.extend({
-		tagName : "tr",
-		template: template
+		className: "btn green boardBtn",
+		template: template,
+		events: {
+			"dragstart": function(e) {
+//				e.dataTransfer.setData("Text",ev.target.id);
+			}
+		},
+		onRender: function() {
+			this.$el.attr("draggable", true);
+			this.$el.css("left", this.model.get("posX"));
+			this.$el.css("top", this.model.get("posY"));
+		}
 	});
 	
 });
