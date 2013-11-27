73f1931f3542b11a627d7e5b95a70e959a1589dc
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2013-11-27 22:03:56 +0000

    Empty view created

--- trunk/src/main/webapp/resources/js/apps/BoardsApp.js
+++ trunk/src/main/webapp/resources/js/apps/BoardsApp.js
@@ -1,23 +1,19 @@
 define(["jquery", "underscore", "backbone", "bootstrap", "marionette", "views/BoardsCompositeView", "collections/BoardsCollection"], function($, _, Backbone, Bootstrap, Marionette, BoardsCompositeView, BoardsCollection) {
 	
-	var BoardsApp = new Marionette.Application();
+	BoardsApp = new Marionette.Application();
 
 	BoardsApp.addRegions({
 		boardsComponentRegion : "#boardsComponentRegion"
 	});
 
 	BoardsApp.on("initialize:after", function() {
-		var boardsCompositeView;
-		var boardsCollection = new BoardsCollection();
-		boardsCollection.fetch({
-			success: function() {
-				boardsCompositeView = new BoardsCompositeView({
-					collection: boardsCollection
-				});
-				
-				BoardsApp.boardsComponentRegion.show(boardsCompositeView);
-			}
+		this.boardsCollection = new BoardsCollection();
+		this.boardsCompositeView = new BoardsCompositeView({
+			collection: this.boardsCollection
 		});
+		
+		BoardsApp.boardsComponentRegion.show(this.boardsCompositeView);
+		this.boardsCollection.fetch();
 
 	});
 
