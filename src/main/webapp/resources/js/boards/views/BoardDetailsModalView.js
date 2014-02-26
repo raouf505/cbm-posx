4ec99e9f0a58c1ae318ecded93e4f48da7b88291
chrisbol82 <chrisbol82@gmail.com>
2014-02-26 22:09:54 +0000

    BoardDetailsModalView updated

--- src/main/webapp/resources/js/boards/views/BoardDetailsModalView.js
+++ src/main/webapp/resources/js/boards/views/BoardDetailsModalView.js
@@ -4,6 +4,25 @@ define(["marionette", "vent", "tpl!boards/templates/boardDetailsModalViewTpl.htm
 		className: "modal fade",
 		template: template,
 		id: "boardDetailsModal",
+		events: {
+			"click #deleteBoardBtn": function() {
+				this.model.destroy();
+			},
+			"click #updateBoardBtn": function() {
+				var boardModel = this.model;
+				var boardName = this.$("#name").val();
+				
+				if(boardModel.collection.findWhere({name: boardName})) {
+					this.$("#updateBoardModalErrorMsg").html("La mesa ya existe.");
+				}
+				else if (!boardModel.save({name: boardName})) {
+					this.$("#updateBoardModalErrorMsg").html(boardModel.validationError);
+				} else {
+					xyz = this;
+					$(this).modal("hide");
+				}
+			}
+		},
 		onRender: function() {
 			var view = this;
 			vent.on("boardButtonView:click", function(model) {
