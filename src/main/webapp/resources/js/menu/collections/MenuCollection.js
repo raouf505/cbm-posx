30e178ff66ffa78cec7501f5ba0570c5648c1eea
chrisbol82 <chrisbol82@gmail.com>
2014-03-27 16:15:29 +0000

    Menu items ordered by number

--- src/main/webapp/resources/js/menu/collections/MenuCollection.js
+++ src/main/webapp/resources/js/menu/collections/MenuCollection.js
@@ -3,7 +3,7 @@ define(["backbone", "menu/models/MenuItemModel"], function(Backbone, MenuItemMod
 	return Backbone.Collection.extend({
 		model: MenuItemModel,
 		url: '/menu/crudService',
-		comparator: "category"
+		comparator: "number"
 	});
 	
 });
