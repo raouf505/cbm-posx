3dfc18e916b042913813dd7787b5fd619bd782e1
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2014-08-18 00:49:40 +0000

    MenuItemAddModalView updated

--- trunk/src/main/webapp/resources/js/menu/views/MenuItemAddModalView.js
+++ trunk/src/main/webapp/resources/js/menu/views/MenuItemAddModalView.js
@@ -1,9 +1,39 @@
-define(['marionette', 'vent', 'tpl!menu/templates/menuItemAddModalViewTpl.html'], function(Marionette, vent, template) {
+define(['marionette', 'vent', 'tpl!menu/templates/menuItemAddModalViewTpl.html', "menu/models/MenuItemModel"], function(Marionette, vent, template, MenuItemModel) {
 	
 	return Marionette.ItemView.extend({
 		id: 'menuItemAddModal',
 		className: 'modal fade',
-		template: template
+		template: template,
+		ui: {
+			addMenuItemForm: "#addMenuItemForm",
+			errorMsg: "#addMenuItemModalErrorMsg"
+		},
+		events: {
+			"submit form": function(event) {
+				event.preventDefault();
+				var menuItemModelData = {
+						number: this.$(addMenuItemForm).find("[name='number']").val(),
+						name: this.$(addMenuItemForm).find("[name='name']").val(),
+						price: this.$(addMenuItemForm).find("[name='price']").val(),
+						category: this.$(addMenuItemForm).find("[name='category']").val()
+				};
+				
+				var menuItemModel = new MenuItemModel();
+				
+				if (!menuItemModel.set(menuItemModelData, {validate: true})) {
+					this.ui.errorMsg.html(menuItemModel.validationError);
+				} else if(this.collection.findWhere({name: menuItemModelData.name}) || this.collection.findWhere({number: menuItemModelData.number})) {
+					this.ui.errorMsg.html("El item ya existe.");
+				} else {
+					this.collection.create(menuItemModel);
+					this.ui.errorMsg.html("");
+					this.$el.modal("hide");
+				}
+			},
+			"click #addMenuItemBtn": function() {
+				this.$(addMenuItemForm).trigger("submit");
+			}
+		},
 	});
 	
 });
