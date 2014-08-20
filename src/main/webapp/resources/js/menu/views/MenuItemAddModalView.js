7436c907d4aba945efcff4f01cc3219c9c98d740
chrisbol82 <chrisbol82@gmail.com>
2014-08-20 04:04:08 +0000

    "number" field removed from menu items

--- src/main/webapp/resources/js/menu/views/MenuItemAddModalView.js
+++ src/main/webapp/resources/js/menu/views/MenuItemAddModalView.js
@@ -12,7 +12,6 @@ define(['marionette', 'vent', 'tpl!menu/templates/menuItemAddModalViewTpl.html',
 			"submit form": function(event) {
 				event.preventDefault();
 				var menuItemModelData = {
-						number: this.$(addMenuItemForm).find("[name='number']").val(),
 						name: this.$(addMenuItemForm).find("[name='name']").val(),
 						price: this.$(addMenuItemForm).find("[name='price']").val(),
 						category: this.$(addMenuItemForm).find("[name='category']").val()
@@ -22,7 +21,7 @@ define(['marionette', 'vent', 'tpl!menu/templates/menuItemAddModalViewTpl.html',
 				
 				if (!menuItemModel.set(menuItemModelData, {validate: true})) {
 					this.ui.errorMsg.html(menuItemModel.validationError);
-				} else if(this.collection.findWhere({name: menuItemModelData.name}) || this.collection.findWhere({number: menuItemModelData.number})) {
+				} else if(this.collection.findWhere({name: menuItemModelData.name}) || this.collection.findWhere({name: menuItemModelData.name})) {
 					this.ui.errorMsg.html("El item ya existe.");
 				} else {
 					this.collection.create(menuItemModel);
