ca542879b90d2979407094ec87b3562888bffa28
chrisbol82@gmail.com <chrisbol82@gmail.com@3b25ed09-7b72-f828-8472-bb2dc44f1519>
2014-09-04 03:55:54 +0000

    MenuItem Multipart request added

--- trunk/src/main/webapp/resources/js/menu/views/MenuItemUpdateModalView.js
+++ trunk/src/main/webapp/resources/js/menu/views/MenuItemUpdateModalView.js
@@ -5,23 +5,44 @@ define(["marionette", "vent", "tpl!menu/templates/menuItemUpdateModalViewTpl.htm
 		className: "modal fade",
 		template: template,
 		events: {
-			"click #updateMenuItemBtn": function() {
-				var view = this;
-				var updatedMenuItemData = {
-						name: this.$("[name='name']").val(),
-						price: this.$("[name='price']").val(),
-						category: this.$("[name='category']").val()
-				};
-				if (!this.model.save(updatedMenuItemData, {success: function() {
-						MenuApp.menuCollection.reset();
-						MenuApp.menuCollection.fetch();
-						console.log(view);
-						view.$el.modal("hide");
-					}})) {
-					this.$("#updateMenuItemModalErrorMsg").html(this.model.validationError);
+			"click .updateMenuItemBtn": function() {
+				if (this.$("form").validateForm()) {
+					this.$el.modal("hide");
+					
+					var action = this.$("form").attr("action");
+					var data = new FormData(this.$("form")[0]);
+					
+					$.ajax({
+						url: action,
+						type: 'POST',
+						data:  data,
+						mimeType: "multipart/form-data",
+						contentType: false,
+					    processData: false,
+					})
+					.done(function( data ) {
+						if ( console && console.log ) {
+							console.log( "Sample of data:", data.slice( 0, 100 ) );
+						}
+					});
 				}
+				
+//				var view = this;
+//				var updatedMenuItemData = {
+//						name: this.$("[name='name']").val(),
+//						price: this.$("[name='price']").val(),
+//						category: this.$("[name='category']").val()
+//				};
+//				if (!this.model.save(updatedMenuItemData, {success: function() {
+//						MenuApp.menuCollection.reset();
+//						MenuApp.menuCollection.fetch();
+//						console.log(view);
+//						view.$el.modal("hide");
+//					}})) {
+//					this.$("#updateMenuItemModalErrorMsg").html(this.model.validationError);
+//				}
 			},
-			"click #deleteMenuItemBtn": function() {
+			"click .deleteMenuItemBtn": function() {
 				this.model.destroy();
 			}
 		},
